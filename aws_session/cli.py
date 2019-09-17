e16569a5ed52ac1dcbb55f7b0896d24cbffa9e46
Bengt Brodersen <bent.brodersen@gmail.com>
2019-09-17 12:17:09 +0200

    feat: add help command

--- aws_session/cli.py
+++ aws_session/cli.py
@@ -8,54 +8,73 @@ from awscli.customizations.configure.writer import ConfigFileWriter
 def argv_get(index):
     return sys.argv[index] if index < len(sys.argv) else None
 
-def main():
-    profile_map = Session().full_config['profiles']
+def printHelp():
+    print("""\
+usage: 
 
-    if argv_get(1) == '-l':
-        ########## List Command ####################################################
-        for profile_name, profile in profile_map.items():
-            if profile.get('role_arn'):
-                print(profile_name)
+refresh session credentials:
+
+    aws-session <profile_name>
+    
+list session profiles:
+
+    aws-session --list/-l
+
+""" )
+
+def refreshSessionCredentials(profile_name):
+    profile_map = Session().full_config['profiles']
+    # ensure profile exists
+    if profile_name not in profile_map:
+        raise ProfileNotFound(profile=profile_name)
+
+    # ensure profile is session profile
+    if 'role_arn' not in profile_map.get(profile_name):
+        raise Exception(f'[{profile_name}] - is not a session profile')
+
+    session = Session(profile=profile_name)
+
+    # setup credentials cache - use aws cli credentials cache
+    credentials_cache_dir = path.expanduser(
+        path.join('~', '.aws', 'cli', 'cache'))
+    credentials_cache = JSONFileCache(credentials_cache_dir)
+    session.get_component('credential_provider') \
+        .get_provider('assume-role').cache = credentials_cache
+
+    # get session credentials
+    session_credentials = session.get_credentials()
+    if not session_credentials:
+        raise NoCredentialsError()
+    session_credentials = session_credentials.get_frozen_credentials()
+
+    # write session credentials to credentials file
+    print(f'[{session.profile}] - set session credentials')
+    credentials_filename = path.expanduser(
+        session.get_config_variable('credentials_file'))
+    ConfigFileWriter().update_config({
+        '__section__': 'test-' + session.profile,
+        'aws_access_key_id': session_credentials.access_key,
+        'aws_secret_access_key': session_credentials.secret_key,
+        'aws_session_token': session_credentials.token
+    }, credentials_filename)
+
+def printSessionProfiles():
+    profile_map = Session().full_config['profiles']
+    for profile_name, profile in profile_map.items():
+        if profile.get('role_arn'):
+            print(profile_name)
+
+def main():
+    if argv_get(1) in ('--help', '-h')  :
+        printHelp()
+    elif argv_get(1) in ('--list', '-l') :
+        printSessionProfiles()
     else:
-        ########## Main Command ####################################################
         profile_name = argv_get(1) \
             or environ.get('AWS_PROFILE') \
             or environ.get('AWS_DEFAULT_PROFILE') \
             or 'default'
-
-        # ensure profile exists
-        if profile_name not in profile_map:
-            raise ProfileNotFound(profile=profile_name)
-
-        # ensure profile is session profile
-        if 'role_arn' not in profile_map.get(profile_name):
-            raise Exception(f'[{profile_name}] - is not a session profile')
-
-        session = Session(profile=profile_name)
-
-        # setup credentials cache - use aws cli credentials cache
-        credentials_cache_dir = path.expanduser(
-            path.join('~', '.aws', 'cli', 'cache'))
-        credentials_cache = JSONFileCache(credentials_cache_dir)
-        session.get_component('credential_provider') \
-            .get_provider('assume-role').cache = credentials_cache
-
-        # get session credentials
-        session_credentials = session.get_credentials()
-        if not session_credentials:
-            raise NoCredentialsError()
-        session_credentials = session_credentials.get_frozen_credentials()
-
-        # write session credentials to credentials file
-        print(f'[{session.profile}] - set session credentials')
-        credentials_filename = path.expanduser(
-            session.get_config_variable('credentials_file'))
-        ConfigFileWriter().update_config({
-            '__section__': 'test-' + session.profile,
-            'aws_access_key_id': session_credentials.access_key,
-            'aws_secret_access_key': session_credentials.secret_key,
-            'aws_session_token': session_credentials.token
-        }, credentials_filename)
+        refreshSessionCredentials(profile_name)
   
 if __name__== "__main__":
   main()
