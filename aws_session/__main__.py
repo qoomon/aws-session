dd99f33d9a465a0ca838c8f24984d5cf343388a6
Bengt Brodersen <bengt.brodersen@gmail.com>
2020-06-02 19:11:25 +0200

    feat: copy source profile config to session profile

--- aws_session/__main__.py
+++ aws_session/__main__.py
@@ -15,11 +15,11 @@ from .configfilewriter import ConfigFileWriter
 
 SESSION_EXPIRATION_THRESHOLD = timedelta(minutes=5)
 
-DEFAULT_SESSION_TOKEN_DURATION_SECONDS = 43200 # 12 hours
-AWS_CONFIG_PATH = path.expanduser(Session().get_config_variable("config_file"))
+DEFAULT_SESSION_TOKEN_DURATION_SECONDS = 43200  # 12 hours
 AWS_CREDENTIALS_PATH = path.expanduser(Session().get_config_variable("credentials_file"))
 
 # ------------------------------------------------------------------------------
+
 
 def handle_help(args):
     print("""\
@@ -60,7 +60,7 @@ def handle_get_session_credentials(args):
     profile_config = Session().full_config["profiles"].get(profile_name)
     if not profile_config:
         raise ProfileNotFound(profile=profile_name)
-        
+
     session_profile_name = f"{profile_name}-session"
     session_profile_config = Session().full_config["profiles"].get(session_profile_name) or {}
 
@@ -68,26 +68,28 @@ def handle_get_session_credentials(args):
     session_expiry_time_value = session_profile_config.get("aws_session_expiry_time")
     if session_expiry_time_value and not session_expiry_time_value == "None":
         session_expiry_time = datetime.strptime(session_expiry_time_value, "%Y-%m-%d %H:%M:%S").astimezone()
-    
+
     session_expiry_duration = session_expiry_time - datetime.now().astimezone()
     if session_expiry_duration < SESSION_EXPIRATION_THRESHOLD or force_new:
-        session_credentials = get_session_credentials(
-            profile_name=profile_name, 
-            profile_config=profile_config
-        )
-        profile_update(AWS_CREDENTIALS_PATH, session_profile_name, {
+        session_credentials = get_session_credentials(profile_name=profile_name, profile_config=profile_config)
+        session_profile_credentials = {
             "aws_access_key_id": session_credentials.access_key,
-            "aws_secret_access_key": session_credentials.secret_key,
+            "aws_secret_access_key" :session_credentials.secret_key,
             "aws_session_token": session_credentials.token,
-            "aws_session_expiry_time": session_credentials.expiry_time.strftime("%Y-%m-%d %H:%M:%S"),
-            "region": profile_config.get("region")
+            "aws_session_expiry_time": session_credentials.expiry_time.strftime("%Y-%m-%d %H:%M:%S")
+        }
+        profile_update(AWS_CREDENTIALS_PATH, session_profile_name, {
+                **session_profile_credentials, # place credentials at top of credential profile
+                **profile_config,
+                **session_profile_credentials  # ensure old credentials are replaced by new ones
         })
         session_expiry_time = session_credentials.expiry_time
         session_expiry_duration = session_expiry_time - datetime.now().astimezone()
-    
+
     print(f"Session profile: {session_profile_name}")
     print(f"Expires in {format_timedelta(session_expiry_duration)}, "
           f"at {session_expiry_time.astimezone().strftime('%Y-%m-%d %H:%M')}")
+
 
 def get_session_credentials(profile_name, profile_config):
     profile_session = Session(profile=profile_name)
@@ -100,22 +102,20 @@ def get_session_credentials(profile_name, profile_config):
             access_key=profile_session_credentials.access_key,
             secret_key=profile_session_credentials.secret_key,
             token=profile_session_credentials.token,
-            expiry_time=profile_session_credentials._expiry_time.astimezone()
-        )
-    else:   
+            expiry_time=profile_session_credentials._expiry_time.astimezone())
+    else:
         print(f"User session")
         session_duration_seconds = profile_config.get('session_duration_seconds') or DEFAULT_SESSION_TOKEN_DURATION_SECONDS
         session_mfa_serial = profile_config.get('session_mfa_serial')
         session_credentials = get_session_token(profile_session,
             DurationSeconds=session_duration_seconds,
-            SerialNumber=session_mfa_serial
-        )['Credentials']
+            SerialNumber=session_mfa_serial)['Credentials']
         return SessionCredentials(
             access_key=session_credentials['AccessKeyId'],
             secret_key=session_credentials['SecretAccessKey'],
             token=session_credentials['SessionToken'],
-            expiry_time=session_credentials['Expiration'].astimezone()
-        )        
+            expiry_time=session_credentials['Expiration'].astimezone())
+
 
 def get_session_token(session, DurationSeconds, SerialNumber):
     sts_client = session.create_client('sts')
@@ -123,18 +123,15 @@ def get_session_token(session, DurationSeconds, SerialNumber):
         return sts_client.get_session_token(
             DurationSeconds=DurationSeconds or DEFAULT_SESSION_TOKEN_DURATION_SECONDS,
             SerialNumber=SerialNumber,
-            TokenCode=getpass(prompt=f"Enter MFA code for {SerialNumber}: ")
-        )
+            TokenCode=getpass(prompt=f"Enter MFA code for {SerialNumber}: "))
     else:
         return sts_client.get_session_token(
-            DurationSeconds=DurationSeconds or DEFAULT_SESSION_TOKEN_DURATION_SECONDS,
-        )
+            DurationSeconds=DurationSeconds or DEFAULT_SESSION_TOKEN_DURATION_SECONDS)
 
 
-class SessionCredentials: 
-    
-    def __init__(self, access_key, secret_key, token, expiry_time): 
-        self.access_key = access_key 
+class SessionCredentials:
+    def __init__(self, access_key, secret_key, token, expiry_time):
+        self.access_key = access_key
         self.secret_key = secret_key
         self.token = token
         self.expiry_time = expiry_time
@@ -161,10 +158,10 @@ def profile_update(config_path, profile_section, config):
         with open(config_path, "a") as config_file:
             config_file.write("\n")
 
-    ConfigFileWriter().update_config({
+    ConfigFileWriter().update_config(config_filename=config_path, new_values={
         **config,
         "__section__": profile_section
-    }, config_path)
+    })
 
 
 def main():
@@ -175,8 +172,8 @@ def main():
 
     parser_command_get = parser_command.add_parser("get", help="Get session credentials")
     parser_command_get.set_defaults(func=handle_get_session_credentials)
-    parser_command_get.add_argument("-p", "--profile", default="default", dest="profile_name", help="Profile name")
-    parser_command_get.add_argument("-f", "--force", action="store_true", dest="force_new", help="Force new session")
+    parser_command_get.add_argument("-p", "--profile", dest="profile_name", default="default", help="Profile name")
+    parser_command_get.add_argument("-f", "--force", dest="force_new", action="store_true", help="Force new session")
 
     parser_command_help = parser_command.add_parser("help", help="Print help")
     parser_command_help.set_defaults(func=handle_help)
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read_text(file_name):
 
 setuptools.setup(
     name='aws-session',
-    version='5.1.0',
+    version='5.2.0',
     author="Bengt Brodersen",
     author_email="me@qoomon.me",
     description="A CLI to generate session credentials based on ~/.aws/config profiles and store them as profile in ~/.aws/credentials file",
